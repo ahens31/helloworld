fc37187c7865a238f2fb0fd704499e2e9be503a7
SooHian <shonendumm@gmail.com>
2017-05-15 22:59:57 +0800

    Change config routes via controller application

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+	def hello
+		render html: "hello momma!"
+	end
+
+	def goodbye
+		render html: "goodbye world!"
+	end
 end
+
+
+
+
